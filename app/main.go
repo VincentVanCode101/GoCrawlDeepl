7e67469216f6a659e22cf885f041481921e06b59
Chris <vincentvancode101@gmail.com>
2024-08-16 10:56:02 +0200

    feat: add retrieval of type of single words

--- app/main.go
+++ app/main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 	"crawl/deepl/utils/browser"
@@ -16,145 +18,196 @@ import (
 	"crawl/deepl/utils/network"
 	"crawl/deepl/utils/telegrambot"
 	"crawl/deepl/utils/url"
-
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
 )
 
 const (
-	baseURL = "https://www.deepl.com/en/translator#"
-	xpath   = `//*[@id="textareasContainer"]/div[3]/section/div[1]/d-textarea/div/p/span`
+	baseURL                             = "https://www.deepl.com/en/translator#"
+	mainTranslationXpath                = `//*[@id="textareasContainer"]/div[3]/section/div[1]/d-textarea/div/p/span`
+	inputSpanForToBeTranslatedWordXpath = `//*[@id="textareasContainer"]/div[1]/section/div/div[1]/d-textarea/div[1]/p/span`
+	typeOfToBeTranslatedWordXpath       = `//*[@id="headlessui-tabs-panel-:R6l8cqbspl:"]/div/div[2]/div/section/div/div/div/div/div/div[1]/span[1]/span[1]`
 )
 
 // Translation holds the phrase to be translated along with its translations.
 type Translation struct {
-	toBeTranslatedPhrase string
-	translatedPhrases    []string
+	Phrase       string
+	Translations map[string]string
 }
 
 // TelegramBot encapsulates the details needed to interact with the Telegram bot API.
 type TelegramBot struct {
-	bot    *tgbotapi.BotAPI
-	chatID int64
+	Bot    *tgbotapi.BotAPI
+	ChatID int64
 }
 
 func main() {
 	totalExecTime := time.Now()
 
+	fromLang, toLang := getLanguages()
+	toBeTranslatedPhrases := getPhrasesToTranslate()
+
+	checkInternetConnection()
+
+	chromeCtx, cancelChrome := setupBrowser()
+	defer cancelChrome()
+
+	telegramBot := setupTelegramBot()
+
+	processTranslations(chromeCtx, fromLang, toLang, toBeTranslatedPhrases, telegramBot)
+
+	fmt.Printf("Total execution time of the program: %v\n\n", time.Since(totalExecTime))
+
+	waitForBrowserClosure()
+}
+
+func getLanguages() (string, string) {
 	fromLang, toLang, err := envutil.GetLanguages()
 	if err != nil {
 		log.Fatalf("Failed to retrieve languages from environment: %v", err)
 	}
+	return fromLang, toLang
+}
 
+func getPhrasesToTranslate() []string {
 	toBeTranslatedPhrases, err := cliargs.FilterNonEmptyArgs()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to get phrases to translate: %v", err)
 	}
+	return toBeTranslatedPhrases
+}
 
+func checkInternetConnection() {
 	if err := network.CheckInternetConnection(); err != nil {
 		log.Fatalf("Failed to establish an internet connection: %v", err)
 	}
+}
 
+func setupBrowser() (context.Context, func()) {
 	keepBrowserOpen := os.Getenv("KEEP_BROWSER_OPEN") == "true"
 	chromeCtx, cancelChrome, cancelExecAllocator, err := browser.GetChromeContext(!keepBrowserOpen)
 	if err != nil {
 		log.Fatalf("Failed to initialize Chrome context: %v", err)
 	}
 
-	defer func() {
+	cancelFunc := func() {
 		if !keepBrowserOpen {
 			cancelExecAllocator()
 			cancelChrome()
 		}
-	}()
+	}
 
+	return chromeCtx, cancelFunc
+}
+
+func setupTelegramBot() *TelegramBot {
 	bot, chatID, err := telegrambot.SetupTelegramBot()
 	if err != nil {
 		log.Printf("Failed to initialize Telegram bot: %v", err)
-		return // the program should continue without the telegram-sending funcionality, thats why we just log and not abort here
+		return nil
 	}
 
-	var telegramBot *TelegramBot
-	if bot != nil {
-		telegramBot = &TelegramBot{bot: bot, chatID: chatID}
-	}
+	return &TelegramBot{Bot: bot, ChatID: chatID}
+}
 
-	fmt.Printf("Amount of unrelated words to translate %d: %s\n", len(toBeTranslatedPhrases), strings.Join(toBeTranslatedPhrases, ", "))
-	for _, toBeTranslatedPhrase := range toBeTranslatedPhrases {
-		handleTranslation(chromeCtx, toBeTranslatedPhrase, fromLang, toLang, telegramBot)
-	}
-
-	fmt.Printf("Total rxecution time of the program: %v\n\n", time.Since(totalExecTime))
-
-	if keepBrowserOpen {
-		fmt.Println("Press 'Enter' to close the browser...")
-		fmt.Scanln()
-
-		cancelExecAllocator()
-		cancelChrome()
+func processTranslations(ctx context.Context, fromLang, toLang string, phrases []string, telegramBot *TelegramBot) {
+	for _, phrase := range phrases {
+		fetchType := !strings.ContainsAny(phrase, " ")
+		translation := translatePhrase(ctx, phrase, fromLang, toLang, fetchType)
+		outputTranslation(translation, telegramBot)
 	}
 }
 
-func handleTranslation(chromeCtx context.Context, toBeTranslatedPhrase string, fromLang string, toLang string, telegrambot *TelegramBot) {
-
+func translatePhrase(ctx context.Context, phrase, fromLang, toLang string, fetchType bool) Translation {
 	startTime := time.Now()
+	defer func() {
+		fmt.Printf("Execution time for translation: %v\n\n", time.Since(startTime))
+	}()
 
 	translation := Translation{
-		toBeTranslatedPhrase: toBeTranslatedPhrase,
-		translatedPhrases:    []string{},
+		Phrase:       phrase,
+		Translations: make(map[string]string),
 	}
 
-	var deeplURL string = url.BuildDeeplURL(baseURL, fromLang, toLang, translation.toBeTranslatedPhrase)
+	deeplURL := url.BuildDeeplURL(baseURL, fromLang, toLang, phrase)
 
-	translatedPhrases, err := runTranslationTask(chromeCtx, deeplURL, xpath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	translation.translatedPhrases = append(translation.translatedPhrases, translatedPhrases...)
+	translation.Translations = fetchTranslation(ctx, deeplURL, fetchType)
 
-	outputTranslation(translation, telegrambot)
-
-	fmt.Printf("Execution time: %v\n\n", time.Since(startTime))
+	return translation
 }
 
-func formatTranslation(input string, translations []string) string {
-	return fmt.Sprintf("Input:\n%s\n\nMain translation:\n%s", input, strings.Join(translations, "\n"))
+func fetchTranslation(ctx context.Context, deeplURL string, fetchType bool) map[string]string {
+	var mainTranslationNodes, typeOfWordNodes []*cdp.Node
+
+	tasks := chromedp.Tasks{
+		chromedp.Navigate(deeplURL),
+		chromedp.WaitVisible(mainTranslationXpath),
+		chromedp.Nodes(mainTranslationXpath, &mainTranslationNodes, chromedp.BySearch),
+	}
+
+	if fetchType {
+		tasks = append(tasks,
+			chromedp.Click(inputSpanForToBeTranslatedWordXpath, chromedp.NodeVisible),
+			chromedp.Nodes(typeOfToBeTranslatedWordXpath, &typeOfWordNodes, chromedp.BySearch),
+		)
+	}
+
+	if err := chromedp.Run(ctx, tasks); err != nil {
+		log.Fatalf("Failed to run translation task: %v", err)
+	}
+
+	translationResult := map[string]string{
+		"mainTranslations": extractTextFromNodes(ctx, mainTranslationNodes),
+	}
+
+	if fetchType {
+		translationResult["typeOfToBeTranslatedWord"] = extractTextFromNodes(ctx, typeOfWordNodes)
+	}
+
+	return translationResult
+}
+
+func extractTextFromNodes(ctx context.Context, nodes []*cdp.Node) string {
+	var texts []string
+	for _, node := range nodes {
+		var text string
+		if err := chromedp.Run(ctx, chromedp.Text(node.FullXPath(), &text)); err != nil {
+			log.Printf("Failed to extract text from node: %v", err)
+			continue
+		}
+		texts = append(texts, text)
+	}
+	return strings.Join(texts, " ")
+}
+
+func formatTranslation(phrase string, translations map[string]string) string {
+	translationText := fmt.Sprintf("Input:\n%s\n\nMain translation:\n%s", phrase, translations["mainTranslations"])
+	if typeOfWord, ok := translations["typeOfToBeTranslatedWord"]; ok {
+		translationText = fmt.Sprintf("Input:\n%s (%s)\n\nMain translation:\n%s", phrase, typeOfWord, translations["mainTranslations"])
+	}
+	return translationText
 }
 
 func outputTranslation(translation Translation, telegramBot *TelegramBot) {
-	text := formatTranslation(translation.toBeTranslatedPhrase, translation.translatedPhrases)
+	text := formatTranslation(translation.Phrase, translation.Translations)
 
 	if telegramBot != nil {
-		msg := tgbotapi.NewMessage(telegramBot.chatID, text)
-		if _, err := telegramBot.bot.Send(msg); err != nil {
-			log.Printf("Failed to send message via Telegram bot: %v", err)
-		}
+		sendTelegramMessage(telegramBot, text)
 	}
+
 	fmt.Println("---------------------------------")
 	fmt.Println(text)
 	fmt.Println("---------------------------------")
 }
 
-func runTranslationTask(ctx context.Context, url, xpath string) ([]string, error) {
-	var nodes []*cdp.Node
-	if err := chromedp.Run(ctx,
-		chromedp.Navigate(url),
-		chromedp.WaitVisible(xpath),
-		chromedp.Nodes(xpath, &nodes, chromedp.BySearch),
-	); err != nil {
-		return nil, err
+func sendTelegramMessage(telegramBot *TelegramBot, text string) {
+	msg := tgbotapi.NewMessage(telegramBot.ChatID, text)
+	if _, err := telegramBot.Bot.Send(msg); err != nil {
+		log.Printf("Failed to send message via Telegram bot: %v", err)
 	}
-	return getTextFromNodes(ctx, nodes)
 }
 
-func getTextFromNodes(ctx context.Context, nodes []*cdp.Node) ([]string, error) {
-	var translatedTexts []string
-	for _, node := range nodes {
-		var text string
-		if err := chromedp.Run(ctx, chromedp.Text(node.FullXPath(), &text)); err != nil {
-			return nil, err
-		}
-		translatedTexts = append(translatedTexts, text)
+func waitForBrowserClosure() {
+	if os.Getenv("KEEP_BROWSER_OPEN") == "true" {
+		fmt.Println("Press 'Enter' to close the browser...")
+		fmt.Scanln()
 	}
-	return translatedTexts, nil
 }
